843af3d1a591ef81443a1565da743a650dda3829
Davi <davifeitosa.dev@protonmail.com>
2021-03-21 18:18:11 -0300

    test: ensure DbAddArticle calls add with correct values

---
+++ src/data/usecases/article/db-add-article.spec.ts
@@ -0,0 +1,59 @@
+import { AddArticleRepository } from '@/data/protocols/add-article-repository';
+import { ArticleModel } from '@/domain/models/article-model';
+import { ArticleParams } from '@/domain/usecases/add-article';
+import {DbAddArticle} from './db-add-article';
+import MockDate from 'mockdate';
+
+const mockArticleParams = (): ArticleParams => ({
+	category: 'any_category',
+	title: 'any_title',
+	body: 'any_body',
+	date: new Date()
+});
+
+const mockArticleModel = (): ArticleModel => ({
+	id: 'any_id',
+	category: 'any_category',
+	title: 'any_title',
+	body: 'any_body',
+	date: new Date()
+});
+
+const makeAddArticleRepository = (): AddArticleRepository => {
+	class AddArticleRepositoryStub implements AddArticleRepository {
+		async add (data: ArticleParams): Promise<ArticleModel> {
+			return Promise.resolve(mockArticleModel());
+		}
+	}
+	return new AddArticleRepositoryStub();
+};
+
+type SutTypes = {
+	addArticleRepositoryStub: AddArticleRepository,
+	sut: DbAddArticle
+}
+
+const makeSut = (): SutTypes => {
+	const addArticleRepositoryStub = makeAddArticleRepository();
+	const sut = new DbAddArticle(addArticleRepositoryStub);
+	return {
+		addArticleRepositoryStub,
+		sut
+	};
+};
+
+describe('DbAddArticle', () => {
+	beforeAll(() => {
+		MockDate.set(new Date());
+	});
+	afterAll(() => {
+		MockDate.reset();
+	});
+
+	test('Should call add with correct values', async () => {
+		const { sut } = makeSut();
+		const addSpy = jest.spyOn(sut, 'add');
+		await sut.add(mockArticleParams());
+		expect(addSpy).toHaveBeenCalledWith(mockArticleParams());
+	});
+});
