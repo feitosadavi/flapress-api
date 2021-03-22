b54a1deecd9570503b1c652f020f4dd7bf386f99
Davi <davifeitosa.dev@protonmail.com>
2021-03-22 15:44:22 -0300

    test: ensure ArticleMongoRepository returns undefined on success

---
+++ src/infra/mongodb/article/article-mongo-repository.spec.ts
@@ -0,0 +1,38 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+import { ArticleMongoRepository } from './article-mongo-repository';
+import {Collection} from 'mongodb';
+import env from '@/config/env';
+
+
+describe('Article Mongo Repository', () => {
+	let articlesCollection: Collection;
+	beforeAll(async () => {
+		await MongoHelper.connect(env.mongoUrl);
+	});
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+	beforeEach(async () => {
+		articlesCollection = await MongoHelper.getCollection('articles');
+		articlesCollection.deleteMany({});
+	});
+
+	const makeSut = () => {
+		return new ArticleMongoRepository();
+	};
+
+	test('Should return undefined on success ', async () => {
+		const sut = makeSut();
+		await sut.add({
+			category: 'any_category',
+			title: 'any_title',
+			body: 'any_body',
+			date: new Date()
+		});
+
+		const article = await articlesCollection.findOne({ title: 'any_title', body: 'any_body' });
+		expect(article._id).toBeTruthy();
+		expect(article.title).toBe('any_title');
+
+	});
+});
