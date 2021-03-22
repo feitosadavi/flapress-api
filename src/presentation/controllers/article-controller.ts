25c99af7e91dbbd286ef9486a1aa787a4e960b84
Davi <davifeitosa.dev@protonmail.com>
2021-03-22 16:18:26 -0300

    feat: ensure ArticleController returns 500 if AddArticle throws

--- src/presentation/controllers/article-controller.ts
+++ src/presentation/controllers/article-controller.ts
@@ -1,12 +1,16 @@
 import { AddArticle } from '@/domain/usecases/add-article';
-import { noContent } from '../helpers/http';
+import { noContent, serverError } from '../helpers/http';
 import { Controller, HttpRequest, HttpResponse } from '../protocols';
 
 export class ArticleController implements Controller {
 	constructor(private readonly addArticle: AddArticle) { }
 
 	async handle (request: HttpRequest): Promise<HttpResponse> {
-		await this.addArticle.add(request.body);
-		return noContent();
+		try {
+			await this.addArticle.add(request.body);
+			return noContent();
+		} catch (error) {
+			return serverError(error);
+		}
 	}
 }
