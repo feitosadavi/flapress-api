1ba0f7ccc2e3978e30303b048b7a12e4d7c36472
Davi <davifeitosa.dev@protonmail.com>
2021-03-22 23:31:53 -0300

    chore: add some scripts, dependencies and fix module alias problem on build

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/**/*-protocols.ts',
-    '!<rootDir>/src/**/protocols/*',
-    '!<rootDir>/src/main/**'
-  ],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1' // captura tudo após @/; $1 é o resultado da captura da expressão regular
-  }
-}
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts',
+		'!<rootDir>/src/**/*-protocols.ts',
+		'!<rootDir>/src/**/protocols/*',
+		'!<rootDir>/src/main/**'
+	],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	preset: '@shelf/jest-mongodb',
+	transform: {
+		'.+\\.ts$': 'ts-jest'
+	},
+	moduleNameMapper: {
+		'@/(.*)': '<rootDir>/src/$1' // captura tudo após @/; $1 é o resultado da captura da expressão regular
+	}
+};
