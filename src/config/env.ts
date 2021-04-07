cdc214230b04b56a340c144623484b77133c82a7
Davi <davifeitosa.dev@protonmail.com>
2021-04-07 00:06:04 -0300

    chore: add admins to env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,5 @@
 export default {
 	mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/flapress-api',
 	port: process.env.PORT || 5050,
+	admins: ['556194252280@c.us', '5561992252342@.us']
 };
