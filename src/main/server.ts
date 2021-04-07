3e5a5053353cf816be5861e331655b96252a9e58
Davi <davifeitosa.dev@protonmail.com>
2021-04-07 00:11:02 -0300

    feat: insert bot at server ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,15 @@
 import 'module-alias/register';
 import env from '@/config/env';
 import { MongoHelper } from '@/infra/mongodb/helpers/mongo-helper';
+import { makeBot } from './factories/makeBot';
 
 MongoHelper.connect(env.mongoUrl)
 	.then(async () => {
 		const app = (await import('./config/app')).default;
 		app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+
+		const bot = await makeBot();
+		bot.start();
+
+		console.log('Bot is running');
 	});
